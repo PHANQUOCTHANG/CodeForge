9bfd409c36e58dc19cbf572353a18b655d94fa0d
Tranv <tranvanphu1882005z@gmail.com>
2025-09-18 19:35:05 +0700

    fresh code immediately

--- CodeForge__FE/vite.config.ts
+++ CodeForge__FE/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // cho phép truy cập từ ngoài container
+    port: 3000, // đổi port thành 3000
+  },
+});
